776118c264fe9def55df5ee351c0665afafdbeb6
Aleksandr Belets <askii.robotics@gmail.com>
2021-01-13 22:27:33 +0300

    update for ubuntu srcipts

--- clean_ubuntu.sh
+++ clean_ubuntu.sh
@@ -2,18 +2,15 @@
 echo "[0%] Cleaning all installed components..."
 echo "[5%] Warning! The BTCU data directory with the wallet will remain untouched."
 echo "[9%] Warning! Will be uninstalled followed libraries: "
-echo ""
 echo "         -   cmake,"
 echo "         -   git,"
 echo "         -   libboost-all-dev,"
 echo "         -   build-essential,"
 echo "         -   libtool,"
-echo "         -   bsdmainutils,"
 echo "         -   autotools-dev,"
 echo "         -   autoconf,"
 echo "         -   pkg-config,"
 echo "         -   automake,"
-echo "         -   python3,"
 echo "         -   libminiupnpc-dev,"
 echo "         -   libzmq3-dev,"
 echo "         -   libssl-dev,"
@@ -37,7 +34,7 @@ echo "         -   Berkeley DB."
 echo ""
 
 # TODO: add prompt and option for silence agreement to keep dependencies: 
-# - base (cmake, git, python3, etc), 
+# - base (cmake, git, etc), 
 # - boost, 
 # - build tools, 
 # - project dependecies, 
@@ -52,133 +49,129 @@ uninstall_package () {
     if [ "install ok installed" = "$PKG_OK" ]; then
     sudo apt remove --purge --auto-remove $REQUIRED_PKG
     fi
+    
+    # clean remaining locks
+    sleep 1
+    sudo killall apt apt-get 2> /dev/null
+    sudo rm /var/lib/apt/lists/lock 2> /dev/null
+    sudo rm /var/cache/apt/archives/lock 2> /dev/null
+    sudo rm /var/lib/dpkg/lock 2> /dev/null
+    sudo rm /var/lib/dpkg/lock-frontend 2> /dev/null
 }
 
 echo "[10%] Uninstalling dependency: cmake... "
 
 uninstall_package cmake
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[12%] Uninstalling dependency: git... "
 
 uninstall_package git
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[14%] Uninstalling dependency: libboost-all-dev... "
 
 uninstall_package libboost-all-dev
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[16%] Uninstalling dependency: build-essential... "
 
 uninstall_package build-essential
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[18%] Uninstalling dependency: libtool... "
 
 uninstall_package libtool
 
-echo -ne  "Done!"
-
-echo "[20%] Uninstalling dependency: bsdmainutils... "
-
-uninstall_package bsdmainutils
-
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[22%] Uninstalling dependency: autotools-dev... "
 
 uninstall_package autotools-dev
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[24%] Uninstalling dependency: autoconf... "
 
 uninstall_package autoconf
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[26%] Uninstalling dependency: pkg-config... "
 
 uninstall_package pkg-config
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[28%] Uninstalling dependency: automake... "
 
 uninstall_package automake
 
-echo -ne  "Done!"
-
-echo "[30%] Uninstalling dependency: python3... "
-
-uninstall_package python3
-
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[32%] Uninstalling dependency: libminiupnpc-dev... "
 
 uninstall_package libminiupnpc-dev
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[34%] Uninstalling dependency: libzmq3-dev... "
 
 uninstall_package libzmq3-dev
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[36%] Uninstalling dependency: librocksdb-dev... "
 
 uninstall_package librocksdb-dev
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[38%] Uninstalling dependency: libssl-dev... "
 
 uninstall_package libssl-dev
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[40%] Uninstalling dependency: libgmp-dev... "
 
 uninstall_package libgmp-dev
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[42%] Uninstalling dependency: libevent-dev... "
 
 uninstall_package libevent-dev
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[44%] Uninstalling dependency: libjsonrpccpp-dev... "
 
 uninstall_package libjsonrpccpp-dev
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[46%] Uninstalling dependency: libsnappy-dev... "
 
 uninstall_package libsnappy-dev
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[48%] Uninstalling dependency: libbenchmark-dev... "
 
 uninstall_package libbenchmark-dev
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[50%] Uninstalling dependency: libgtest-dev... "
 
 uninstall_package libgtest-dev
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[52%] Uninstalling Berkeley DB... "
 
@@ -189,7 +182,9 @@ then
     make
     sudo make uninstall
     cd -
-fi;
+else
+    echo  "Berkeley DB is not installed."
+fi
 
 echo  "Done!"
 
@@ -197,25 +192,25 @@ echo "[54%] Uninstalling dependency: libprotobuf-dev... "
 
 uninstall_package libprotobuf-dev
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[56%] Uninstalling dependency: protobuf-compiler... "
 
 uninstall_package protobuf-compiler
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[58%] Uninstalling dependency: libqrencode-dev... "
 
 uninstall_package libqrencode-dev
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[60%] Uninstalling dependency: libpng-dev... "
 
 uninstall_package libpng-dev
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[62%] Uninstalling QT Components. "
 
@@ -223,43 +218,43 @@ echo "[64%] Uninstalling QT dependency: libqt5gui5... "
 
 uninstall_package libqt5gui5
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[66%] Uninstalling QT dependency: libqt5core5a... "
 
-install_package libqt5core5a
+uninstall_package libqt5core5a
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[68%] Uninstalling QT dependency: libqt5dbus5... "
 
 uninstall_package libqt5dbus5
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[70%] Uninstalling QT dependency: qttools5-dev... "
 
 uninstall_package qttools5-dev
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[70%] Uninstalling QT dependency: qttools5-dev-tools... "
 
 uninstall_package qttools5-dev-tools
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[72%] Uninstalling QT dependency: libqt5svg5-dev... "
 
 uninstall_package libqt5svg5-dev
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[74%] Uninstalling QT dependency: libqt5charts5-dev... "
 
 uninstall_package libqt5charts5-dev
 
-echo -ne  "Done!"
+echo  "Done!"
 
 echo "[80%] All QT Components has been uninstalled. "
 
@@ -267,20 +262,17 @@ echo "[90%] Removing of the BTCU sources... "
 
 if [[ -d ./src/qt/btcu ]]
 then
-    echo "Current filder is BTCU project folder. Starting cleaning... "
-    mv clean_ubuntu.sh /tmp/
-    mv install_ubuntu.sh /tmp/
+    echo "Current filder IS the BTCU project folder. Starting cleaning... "
+    mv clean_ubuntu.sh /tmp/ 2> /dev/null
+    mv install_ubuntu.sh /tmp/ 2> /dev/null
     rm -rf ./*
-    mv /tmp/clean_ubuntu.txt ./
-    mv /tmp/install_ubuntu.txt ./
+    mv /tmp/clean_ubuntu.sh ./ 2> /dev/null
+    mv /tmp/install_ubuntu.sh ./ 2> /dev/null
 else 
-    echo "Current filder is not BTCU project folder. Stopping...  "
+    echo "Current filder IS NOT the BTCU project folder. Stopping...  "
 fi
 
 echo  "Done!"
-
-echo ""
-echo ""
-echo ""
-
-echo  "[100%] The cleanising process is completed!"
+echo  ""
+echo  "[100%] The cleaning process is completed!"
+echo  "You can reinstall the BTCU project via install_ubuntu.sh script."
--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-echo "[0%] Updating apt-get..."
+echo  "[0%] Updating apt-get..."
 sudo apt-get update 
-echo "[5%] Upgrading apt-get..."
-sudo apt-get upgrade
-echo "[10%] Finished."
+echo  "[0%] Updating apt-get... Done!"
+echo  "[5%] Upgrading apt-get..."
+sudo apt-get upgrade --assume-yes --force-yes
+echo  "[5%] Upgrading apt-get... Done!"
+echo  "[10%] Finished."
 
 install_package () {
     REQUIRED_PKG="$1"
     PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
     if [ "" = "$PKG_OK" ]; then
-    sudo apt-get --yes install $REQUIRED_PKG 
+    sudo apt-get --assume-yes --force-yes install $REQUIRED_PKG 
+    else 
+    echo "Already installed."
     fi
 }
 
@@ -17,229 +21,309 @@ uninstall_package () {
     REQUIRED_PKG="$1"
     PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
     if [ "install ok installed" = "$PKG_OK" ]; then
-    sudo apt remove --purge --auto-remove $REQUIRED_PKG
+    sudo apt remove --purge --auto-remove --assume-yes --force-yes $REQUIRED_PKG
     fi
+    
+    # clean remaining locks
+    sleep 1
+    sudo killall apt apt-get 2> /dev/null
+    sudo rm /var/lib/apt/lists/lock 2> /dev/null
+    sudo rm /var/cache/apt/archives/lock 2> /dev/null
+    sudo rm /var/lib/dpkg/lock 2> /dev/null
+    sudo rm /var/lib/dpkg/lock-frontend 2> /dev/null
 }
 
-echo "[11%] Installing dependency: cmake... "
+echo  ""
+echo  "[11%] Installing dependency: cmake... "
 
 install_package cmake
 
-echo -ne  "Done!"
+echo  ""
+echo  "[11%] Installing dependency: cmake... Done!"
 
-echo "[12%] Installing dependency: git... "
+echo  ""
+echo  "[12%] Installing dependency: git... "
 
 install_package git
 
-echo -ne  "Done!"
+echo  ""
+echo  "[12%] Installing dependency: git... Done!"
 
-echo "[13%] Installing dependency: libboost-all-dev... "
+echo  ""
+echo  "[13%] Installing dependency: libboost-all-dev... "
 
 install_package libboost-all-dev
 
-echo -ne  "Done!"
+echo  ""
+echo  "[13%] Installing dependency: libboost-all-dev... Done!"
 
-echo "[14%] Installing dependency: build-essential... "
+echo  ""
+echo  "[14%] Installing dependency: build-essential... "
 
 install_package build-essential
 
-echo -ne  "Done!"
+echo  ""
+echo  "[14%] Installing dependency: build-essential... Done!"
 
-echo "[15%] Installing dependency: libtool... "
+echo  ""
+echo  "[15%] Installing dependency: libtool... "
 
 install_package libtool
 
-echo -ne  "Done!"
+echo  ""
+echo  "[15%] Installing dependency: libtool... Done!"
 
-echo "[16%] Installing dependency: bsdmainutils... "
+echo  ""
+echo  "[16%] Installing dependency: bsdmainutils... "
 
 install_package bsdmainutils
 
-echo -ne  "Done!"
+echo  ""
+echo  "[16%] Installing dependency: bsdmainutils... Done!"
 
-echo "[17%] Installing dependency: autotools-dev... "
+echo  ""
+echo  "[17%] Installing dependency: autotools-dev... "
 
 install_package autotools-dev
 
-echo -ne  "Done!"
+echo  ""
+echo  "[17%] Installing dependency: autotools-dev... Done!"
 
-echo "[18%] Installing dependency: autoconf... "
+echo  ""
+echo  "[18%] Installing dependency: autoconf... "
 
 install_package autoconf
 
-echo -ne  "Done!"
+echo  ""
+echo  "[18%] Installing dependency: autoconf... Done!"
 
-echo "[19%] Installing dependency: pkg-config... "
+echo  ""
+echo  "[19%] Installing dependency: pkg-config... "
 
 install_package pkg-config
 
-echo -ne  "Done!"
+echo  ""
+echo  "[19%] Installing dependency: pkg-config... Done!"
 
-echo "[20%] Installing dependency: automake... "
+echo  ""
+echo  "[20%] Installing dependency: automake... "
 
 install_package automake
 
-echo -ne  "Done!"
+echo  ""
+echo  "[20%] Installing dependency: automake... Done!"
 
-echo "[21%] Installing dependency: python3... "
+echo  ""
+echo  "[21%] Installing dependency: python3... "
 
 install_package python3
 
-echo -ne  "Done!"
+echo  ""
+echo  "[21%] Installing dependency: python3... Done!"
 
-echo "[22%] Installing dependency: libminiupnpc-dev... "
+echo  ""
+echo  "[22%] Installing dependency: libminiupnpc-dev... "
 
 install_package libminiupnpc-dev
 
-echo -ne  "Done!"
+echo  ""
+echo  "[22%] Installing dependency: libminiupnpc-dev... Done!"
 
-echo "[23%] Installing dependency: libzmq3-dev... "
+echo  ""
+echo  "[23%] Installing dependency: libzmq3-dev... "
 
 install_package libzmq3-dev
 
-echo -ne  "Done!"
+echo  ""
+echo  "[23%] Installing dependency: libzmq3-dev... Done!"
 
-echo "[24%] Installing dependency: librocksdb-dev... "
+echo  ""
+echo  "[24%] Installing dependency: librocksdb-dev... "
 
 install_package librocksdb-dev
 
-echo -ne  "Done!"
+echo  ""
+echo  "[24%] Installing dependency: librocksdb-dev... Done!"
 
-echo "[25%] Installing dependency: libssl-dev... "
+echo  ""
+echo  "[25%] Installing dependency: libssl-dev... "
 
 install_package libssl-dev
 
-echo -ne  "Done!"
+echo  ""
+echo  "[25%] Installing dependency: libssl-dev... Done!"
 
-echo "[26%] Installing dependency: libgmp-dev... "
+echo  ""
+echo  "[26%] Installing dependency: libgmp-dev... "
 
 install_package libgmp-dev
 
-echo -ne  "Done!"
+echo  ""
+echo  "[26%] Installing dependency: libgmp-dev... Done!"
 
-echo "[27%] Installing dependency: libevent-dev... "
+echo  ""
+echo  "[27%] Installing dependency: libevent-dev... "
 
 install_package libevent-dev
 
-echo -ne  "Done!"
+echo  ""
+echo  "[27%] Installing dependency: libevent-dev... Done!"
 
-echo "[28%] Installing dependency: libjsonrpccpp-dev... "
+echo  ""
+echo  "[28%] Installing dependency: libjsonrpccpp-dev... "
 
 install_package libjsonrpccpp-dev
 
-echo -ne  "Done!"
+echo  ""
+echo  "[28%] Installing dependency: libjsonrpccpp-dev... Done!"
 
-echo "[29%] Installing dependency: libsnappy-dev... "
+echo  ""
+echo  "[29%] Installing dependency: libsnappy-dev... "
 
 install_package libsnappy-dev
 
-echo -ne  "Done!"
+echo  ""
+echo  "Done!"
 
-echo "[30%] Installing dependency: libbenchmark-dev... "
+echo  ""
+echo  "[30%] Installing dependency: libbenchmark-dev... "
 
 install_package libbenchmark-dev
 
-echo -ne  "Done!"
+echo  ""
+echo  "[30%] Installing dependency: libbenchmark-dev... Done!"
 
-echo "[31%] Installing dependency: libgtest-dev... "
+echo  ""
+echo  "[31%] Installing dependency: libgtest-dev... "
 
 install_package libgtest-dev
 
-echo -ne  "Done!"
+echo  ""
+echo  "[31%] Installing dependency: libgtest-dev... Done!"
+echo  ""
 
-echo "[32%] Configuring GTest... "
+echo  "[32%] Configuring GTest... "
 
 cd /usr/src/googletest
 sudo cmake .
 sudo cmake --build . --target install
 cd -
 
-echo "Done!"
+echo  ""
+echo  "[32%] Configuring GTest... Done!"
 
-echo "[32%] Checking Berkeley DB... "
+echo  ""
+echo  "[32%] Checking Berkeley DB... "
 
 uninstall_package libdb-dev
 
 uninstall_package libdb++-dev
 
-echo -ne  "Done!"
+echo  ""
+echo  "[32%] Checking Berkeley DB... Done!"
 
-echo "[33%] Installing dependency: libprotobuf-dev... "
+echo  ""
+echo  "[33%] Installing dependency: libprotobuf-dev... "
 
 install_package libprotobuf-dev
 
-echo -ne  "Done!"
+echo  ""
+echo  "[33%] Installing dependency: libprotobuf-dev... Done!"
 
-echo "[34%] Installing dependency: protobuf-compiler... "
+echo  ""
+echo  "[34%] Installing dependency: protobuf-compiler... "
 
 install_package protobuf-compiler
 
-echo -ne  "Done!"
+echo  ""
+echo  "[34%] Installing dependency: protobuf-compiler... Done!"
 
-echo "[35%] Installing dependency: libqrencode-dev... "
+echo  ""
+echo  "[35%] Installing dependency: libqrencode-dev... "
 
 install_package libqrencode-dev
 
-echo -ne  "Done!"
+echo  ""
+echo  "[35%] Installing dependency: libqrencode-dev... Done!"
 
-echo "[36%] Installing dependency: libpng-dev... "
+echo  ""
+echo  "[36%] Installing dependency: libpng-dev... "
 
 install_package libpng-dev
 
-echo -ne  "Done!"
+echo  ""
+echo  "[36%] Installing dependency: libpng-dev... Done!"
 
-echo "[39%] Installing QT Components. "
+echo  ""
+echo  "[39%] Installing QT Components. "
 
-echo "[40%] Installing QT dependency: libqt5gui5... "
+echo  ""
+echo  "[40%] Installing QT dependency: libqt5gui5... "
 
 install_package libqt5gui5
 
-echo -ne  "Done!"
+echo  ""
+echo  "[40%] Installing QT dependency: libqt5gui5... Done!"
 
-echo "[41%] Installing QT dependency: libqt5core5a... "
+echo  ""
+echo  "[41%] Installing QT dependency: libqt5core5a... "
 
 install_package libqt5core5a
 
-echo -ne  "Done!"
+echo  ""
+echo  "[41%] Installing QT dependency: libqt5core5a... Done!"
 
-echo "[42%] Installing QT dependency: libqt5dbus5... "
+echo  ""
+echo  "[42%] Installing QT dependency: libqt5dbus5... "
 
 install_package libqt5dbus5
 
-echo -ne  "Done!"
+echo  ""
+echo  "[42%] Installing QT dependency: libqt5dbus5... Done!"
 
-echo "[43%] Installing QT dependency: qttools5-dev... "
+echo  ""
+echo  "[43%] Installing QT dependency: qttools5-dev... "
 
 install_package qttools5-dev
 
-echo -ne  "Done!"
+echo  ""
+echo  "[43%] Installing QT dependency: qttools5-dev... Done!"
 
-echo "[44%] Installing QT dependency: qttools5-dev-tools... "
+echo  ""
+echo  "[44%] Installing QT dependency: qttools5-dev-tools... "
 
 install_package qttools5-dev-tools
 
-echo -ne  "Done!"
+echo  ""
+echo  "[44%] Installing QT dependency: qttools5-dev-tools... Done!"
 
-echo "[45%] Installing QT dependency: libqt5svg5-dev... "
+echo  ""
+echo  "[45%] Installing QT dependency: libqt5svg5-dev... "
 
 install_package libqt5svg5-dev
 
-echo -ne  "Done!"
+echo  ""
+echo  "[45%] Installing QT dependency: libqt5svg5-dev... Done!"
 
-echo "[46%] Installing QT dependency: libqt5charts5-dev... "
+echo  ""
+echo  "[46%] Installing QT dependency: libqt5charts5-dev... "
 
 install_package libqt5charts5-dev
 
-echo -ne  "Done!"
+echo  ""
+echo  "[46%] Installing QT dependency: libqt5charts5-dev... Done!"
 
-echo "[47%] All QT Components has been installed. "
+echo  ""
+echo  "[47%] All QT Components has been installed. "
 
-echo "[50%] Checking is folder the git repository... "
+echo  ""
+echo  "[50%] Checking is folder the git repository... "
 if [ -d .git ]; then
 echo -ne  "yes"
     if [ "$1" = "update" ]
     then
-    echo "[50%] Updating current version of the BTCU"
+    echo  ""
+    echo  "[50%] Updating current version of the BTCU... "
     git checkout master 
 
     if [ -s "versions.txt" ]
@@ -253,79 +337,99 @@ echo -ne  "yes"
             done <"$file"
 
             my_var="$( cut -d ' ' -f 2 <<< "$l" )";
-            echo "[51%] Working branch: release_$my_var"
+            echo  ""
+            echo  "[51%] Working branch: release_$my_var"
             git checkout "release_$my_var"
         else
-            echo "[51%] Working branch: master"
+            echo  ""
+            echo  "[51%] Working branch: master"
         fi
 
     git pull
-    echo  "Done!"
-
+    echo  ""
+    echo  "[50%] Updating current version of the BTCU... Done!"
+    echo  ""
+    
     else 
-    echo "[50%] Updating current version of the BTCU"
-    echo "[51%] Working branch: master"
+    
+    echo  ""
+    echo  "[50%] Updating current version of the BTCU"
+    
+    echo  ""
+    echo  "[51%] Working branch: master"
     git checkout master 
     git pull
-    echo  "Done!"
+    echo  ""
+    echo  "[51%] Working branch: master... Done!"
+    echo  ""
     fi
 else
     echo -ne  "no"
-    echo "[50%] Downloading latest version of the BTCU... "
-    git clone https://github.com/btcu-ultimatum/btcu
+    echo  ""
+    echo  "[50%] Downloading latest version of the BTCU... "
+    git clone https://github.com/askiiRobotics/btcu
     cd btcu
-    echo  "Done!"
+    sudo git switch lunix-build
+    echo  ""
+    echo  "[50%] Downloading latest version of the BTCU... Done!"
+    echo  ""
 fi;
 
-echo "[60%] Installing Berkeley DB... "
+echo  ""
+echo  "[60%] Installing Berkeley DB... "
 
-# Since as of 5th March 2020 the Oracle moved Barkeley DB 
-# to login-protected tarball for 18.1.32 version 
-# we added the dependency as a static file included in the repository.
-# You can check the details in depends/packages/static/berkeley-db-18.1.32/README.MD
-
-tar zxvf depends/packages/static/berkeley-db-18.1.32/berkeley-db-18.1.32.tar.gz -C ./
-cd  db-18.1.32/build_unix
-../dist/configure --enable-cxx --disable-shared --disable-replication --with-pic --prefix=/opt
-make
 
 if [ -f /opt/lib/libdb-18.1.a ]
 then
-    sudo make uninstall
-fi;
+    echo  "[60%] Berkeley DB is already installed."
+else
+    # Since as of 5th March 2020 the Oracle moved Barkeley DB 
+    # to login-protected tarball for 18.1.32 version 
+    # we added the dependency as a static file included in the repository.
+    # You can check the details in depends/packages/static/berkeley-db-18.1.32/README.MD
 
-sudo make install
-cd -
+    tar zxvf depends/packages/static/berkeley-db-18.1.32/berkeley-db-18.1.32.tar.gz -C ./
+    cd  db-18.1.32/build_unix
+    ../dist/configure --enable-cxx --disable-shared --disable-replication --with-pic --prefix=/opt
+    make
+    sudo make install
+    cd -
+fi
 
-echo  "Done!"
+echo  ""
+echo  "[60%] Installing Berkeley DB... Done!"
 
-
-echo "[65%] Running CMake configuring... "
+echo  ""
+echo  "[65%] Running CMake configuring... "
 
 cmake -G "CodeBlocks - Unix Makefiles" .
 
-echo  "Done!"
+echo  ""
+echo  "[65%] Running CMake configuring... Done!"
 
-echo "[75%] Building BTCU... "
+echo  ""
+echo  "[75%] Building BTCU... "
 
 make
 
-echo  "Done!"
+echo  ""
+echo  "[75%] Building BTCU... Done!"
 
-echo "[100%] Build is completed!"
+echo  ""
+echo  "[100%] Build is completed!"
 
-echo ""
-echo ""
-echo ""
+echo  ""
+echo  ""
+echo  ""
 
-echo "=========================================================="
-echo "The built binaries was placed in ./bin folder"
-echo "For start daemon please run:"
-echo "./bin/btcud -daemon"
-echo "Outputs a list of command-line options:"
-echo "./bin/btcu-cli --help"
-echo "Outputs a list of RPC commands when the daemon is running:"
-echo "./bin/btcu-cli help"
-echo "Start GUI:"
-echo "./src/qt/btcu-qt"
-echo "=========================================================="
+echo  "=========================================================="
+echo  "The built binaries was placed in ./bin folder"
+echo  "For start daemon please run:"
+echo  "./bin/btcud -daemon"
+echo  "Outputs a list of command-line options:"
+echo  "./bin/btcu-cli --help"
+echo  "Outputs a list of RPC commands when the daemon is running:"
+echo  "./bin/btcu-cli help"
+echo  "Start GUI:"
+echo  "./src/qt/btcu-qt"
+echo  "=========================================================="
