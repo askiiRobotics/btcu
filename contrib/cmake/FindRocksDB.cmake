f1a44495247605a812652b7944d95f816c566aad
Aleksandr Belets <askii.robotics@gmail.com>
2021-01-26 22:55:55 +0300

    update rocksdb cmake config

--- contrib/cmake/FindRocksDB.cmake
+++ contrib/cmake/FindRocksDB.cmake
@@ -4,20 +4,36 @@
 #  ROCKSDB_ROOT_DIR:    Base directory where all RocksDB components are found
 #
 # The following are set after configuration is done:
-#  rocksdb_FOUND
+#  RocksDB_FOUND
 #  RocksDB_INCLUDE_DIR
 #  ROCKSDB_LIBRARY
 
+message(STATUS "Into rocksdb cmake find")
 find_path(RocksDB_INCLUDE_DIR NAMES rocksdb/db.h
                              PATHS ${ROCKSDB_ROOT_DIR} ${ROCKSDB_ROOT_DIR}/include /usr/include /usr/local/include)
 
-find_library(ROCKSDB_LIBRARY NAMES rocksdb rocksdbd librocksdb
-                              PATHS ${ROCKSDB_ROOT_DIR} ${ROCKSDB_ROOT_DIR}/lib /usr/lib /usr/local/lib)
+message(STATUS RocksDB_INCLUDE_DIR:)
+message(STATUS ${RocksDB_INCLUDE_DIR})
+
+if(RocksDB_INCLUDE_DIR)
+	find_component(RocksDB rocksdb
+		NAMES
+            rocksdb rocksdbd librocksdb
+			libzmq
+		INCLUDE_DIRS ${RocksDB_INCLUDE_DIR}
+	)
+endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(RocksDB DEFAULT_MSG RocksDB_INCLUDE_DIR ROCKSDB_LIBRARY)
+find_package_handle_standard_args(RocksDB
+	REQUIRED_VARS RocksDB_INCLUDE_DIR
+	HANDLE_COMPONENTS
+)
 
-if(rocksdb_FOUND)
+if(RocksDB_FOUND)
+  set(ZeroMQ_INCLUDE_DIR "${RocksDB_INCLUDE_DIRS}")
+  set(ROCKSDB_LIBRARY "${RocksDB_LIBRARIES}")
+
   message(STATUS "Found RocksDB  (include: ${RocksDB_INCLUDE_DIR}, library: ${ROCKSDB_LIBRARY})")
   mark_as_advanced(RocksDB_INCLUDE_DIR ROCKSDB_LIBRARY)
 endif()
