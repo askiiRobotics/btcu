12e47099c2270ba8346b6e90d1612b5fd17ce09f
Aleksandr Belets <askii.robotics@gmail.com>
2021-01-30 18:00:21 +0300

    updated scripts

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -41,7 +41,7 @@ build=1
 wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
 tar -xzvf cmake-$version.$build.tar.gz
 cd cmake-$version.$build/
-./bootstrap
+./bootstrap --prefix=/usr
 make -j$(nproc)
 sudo make install
 cd -
